a99839f08ccb2694f11c69a97ccf6bad4ccbfda5
unknown <ramovic225@gmail.com>
2025-08-13 17:11:58 +0200

    homepage style changes

--- src/app/[locale]/(black-layout)/page.js
+++ src/app/[locale]/(black-layout)/page.js
@@ -1,6 +1,9 @@
+"use client";
+
 import TypewriterText from "@/components/Typewriter";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { delay, motion } from "framer-motion";
 
 function HomePage() {
   const t = useTranslations("HomePage");
@@ -15,23 +18,43 @@ function HomePage() {
           height={1341}
         />
       </div> */}
-      <h1 className="mb-12 md:mb-32">{t("ko_smo_mi")}</h1>
+      <motion.h1
+        initial={{ opacity: 0, x: -150 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="mb-12 md:mb-32"
+      >
+        {t("ko_smo_mi")}
+      </motion.h1>
       <div className="flex flex-col xl:flex-row justify-between w-full">
         <TypewriterText
           lines={["Global", "Design", "Agency."]}
           colors={["text-white", "text-white/85", "text-white/70"]}
+          textClassName={
+            "inline-block w-[10ch] font-bold  text-6xl md:text-[130px] uppercase tracking-widest leading-none relative"
+          }
         />
         <div className="flex flex-col justify-between">
-          <p className="flex-1 flex justify-end pt-16 md:pt-0 md:text-lg">
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3, delay: 3.5 }}
+            className="flex-1 flex justify-end pt-16 md:pt-0 md:text-lg"
+          >
             Branding & Creative Direction / Product and Packaging,
             <br />
             Editorial & Photography / Digital Media & Animation /
             <br />
             Spatial Design & Experiential
-          </p>
-          <p className="flex justify-end border-r border-gray-500 text-xl pr-8 mt-12 leading-12">
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, x: 150 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 3 }}
+            className="flex justify-end border-r border-gray-500 text-xl pr-8 mt-12 leading-12"
+          >
             {t("sta_radimo")}
-          </p>
+          </motion.p>
         </div>
       </div>
     </div>
--- src/components/Typewriter.jsx
+++ src/components/Typewriter.jsx
@@ -7,54 +7,82 @@ const TypewriterText = ({
   speed = 100,
   delayBetween = 100,
   colors = [],
+  textClassName = "",
+  startDelay = 1000,
 }) => {
   const [currentLine, setCurrentLine] = useState(0);
   const [displayedLines, setDisplayedLines] = useState([]);
   const [charIndex, setCharIndex] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+  const [isFirstRun, setIsFirstRun] = useState(true);
 
   useEffect(() => {
     if (currentLine >= lines.length) return;
 
     const line = lines[currentLine];
 
-    const timeout = setTimeout(
-      () => {
-        if (charIndex < line.length) {
-          const updatedLines = [...displayedLines];
-          updatedLines[currentLine] =
-            (updatedLines[currentLine] || "") + line[charIndex];
-          setDisplayedLines(updatedLines);
-          setCharIndex(charIndex + 1);
-        } else {
-          setCurrentLine(currentLine + 1);
-          setCharIndex(0);
-        }
-      },
-      charIndex < line.length ? speed : delayBetween
-    );
+    const delay = isFirstRun
+      ? startDelay
+      : charIndex < line.length
+      ? speed
+      : delayBetween;
+
+    const timeout = setTimeout(() => {
+      if (charIndex < line.length) {
+        if (!isStarted) setIsStarted(true);
+        const updatedLines = [...displayedLines];
+        updatedLines[currentLine] =
+          (updatedLines[currentLine] || "") + line[charIndex];
+        setDisplayedLines(updatedLines);
+        setCharIndex(charIndex + 1);
+      } else {
+        setCurrentLine(currentLine + 1);
+        setCharIndex(0);
+      }
+      setIsFirstRun(false);
+    }, delay);
 
     return () => clearTimeout(timeout);
-  }, [charIndex, currentLine, lines, displayedLines, speed, delayBetween]);
+  }, [
+    charIndex,
+    currentLine,
+    lines,
+    displayedLines,
+    speed,
+    delayBetween,
+    isFirstRun,
+  ]);
 
   return (
     <p className="flex-1 flex flex-col">
-      {lines.map((line, index) => (
-        <span
-          key={index}
-          className={`inline-block w-[10ch] font-bold  text-6xl md:text-[130px] uppercase tracking-widest leading-none relative ${
-            colors[index] || ""
-          }`}
-        >
-          <span className="invisible">{line}</span>
+      {lines.length === 1 ? (
+        <span className={`${textClassName} ${colors[0] || ""}`}>
+          <span className="invisible">{lines[0]}</span>
 
           <span className="absolute left-0 top-0">
-            {displayedLines[index] || ""}
-            {index === currentLine && (
+            {displayedLines[0] || ""}
+            {currentLine === 0 && (
               <span className="w-12 animate-ping !font-light">|</span>
             )}
           </span>
         </span>
-      ))}
+      ) : (
+        lines.map((line, index) => (
+          <span
+            key={index}
+            className={`${textClassName} ${colors[index] || ""}`}
+          >
+            <span className="invisible">{line}</span>
+
+            <span className="absolute left-0 top-0">
+              {displayedLines[index] || ""}
+              {index === currentLine && isStarted && (
+                <span className="w-12 animate-ping !font-light">|</span>
+              )}
+            </span>
+          </span>
+        ))
+      )}
     </p>
   );
 };
