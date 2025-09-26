d929cc6dc2af651378cea085b02d80549b91826b
unknown <ramovic225@gmail.com>
2025-09-26 20:15:35 +0200

    error

--- src/app/api/about/route.js
+++ src/app/api/about/route.js
@@ -8,7 +8,7 @@ export async function GET() {
   } catch (error) {
     console.error("Error fetching AboutUs:", error);
     return NextResponse.json(
-      { error: "Failed to fetch content" },
+      { error: "Failed to fetch content:", error },
       { status: 500 }
     );
   }
