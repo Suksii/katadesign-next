bfb6557b2d1d8ec6125e63ad00909b7225354737
unknown <ramovic225@gmail.com>
2025-09-28 19:35:07 +0200

    model, endpoint and methods for services added

--- src/app/api/services/route.js
+++ src/app/api/services/route.js
@@ -1,6 +1,20 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    console.log("prisma.services:", prisma.services);
+    const services = await prisma.services.findMany();
+    return NextResponse.json(services);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Greška pri čitanju usluga:", error },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(req) {
   try {
     const body = await req.json();
@@ -13,15 +27,65 @@ export async function POST(req) {
       return NextResponse.json({ error: "Naziv usluge je obavezan" });
     }
 
-    const category = await prisma.services.create({
+    const service = await prisma.services.create({
       data: { titleMn, titleEn, contentMn, contentEn },
     });
 
-    return NextResponse.json(category, { status: 201 });
+    return NextResponse.json(service, { status: 201 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
       { error: "Greška pri dodavanju usluge:", error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { id } = await req.json();
+
+    if (!id) {
+      return NextResponse.json(
+        { error: "ID usluge je obavezan" },
+        { status: 400 }
+      );
+    }
+
+    await prisma.services.delete({
+      where: { id: Number(id) },
+    });
+
+    return NextResponse.json({ message: "Usluga obrisana" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Greška pri brisanju usluge", details: error },
+      { status: 500 }
+    );
+  }
+}
+export async function PUT(req) {
+  try {
+    const { id, titleMn, titleEn, contentMn, contentEn } = await req.json();
+
+    if (!id || !titleMn || !titleEn || !contentMn || !contentEn) {
+      return NextResponse.json(
+        { error: "Svi podaci su obavezni" },
+        { status: 400 }
+      );
+    }
+
+    const updatedService = await prisma.services.update({
+      where: { id },
+      data: { titleMn, titleEn, contentMn, contentEn },
+    });
+
+    return NextResponse.json(updatedService, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Greška pri izmjeni usluge" },
       { status: 500 }
     );
   }
