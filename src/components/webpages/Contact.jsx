cf3b4b976693302cb311f2581e26c3bbdf7d8867
unknown <ramovic225@gmail.com>
2025-09-28 22:44:08 +0200

    contact changed

--- src/components/webpages/Contact.jsx
+++ src/components/webpages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className="flex gap-12 flex-col md:flex-row md:pt-12">
       <div className="flex-1 flex flex-col justify-between gap-6">
-        <p className="pb-8 md:pb-0">email@kata.design</p>
+        <p className="pb-8 md:pb-0">archivekata@gmail.com</p>
         <div className="flex justify-between md:w-[80%]">
           <div className="flex flex-col gap-4">
             <p className="uppercase font-medium">{t("pronadjite_nas")}</p>
@@ -19,7 +19,7 @@ const Contact = () => {
           </div>
           <div className="flex flex-col gap-2">
             <p className="font-medium">{t("crna_gora_evropa")}</p>
-            <p className="font-medium">+382 1234567</p>
+            <p className="font-medium">+382 69 555 622</p>
           </div>
         </div>
       </div>
