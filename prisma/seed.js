71c3faa199017fabfb0761bdee078bc8c99db891
unknown <ramovic225@gmail.com>
2025-10-05 22:11:15 +0200

    admin model added and seed created

---
+++ prisma/seed.js
@@ -0,0 +1,31 @@
+require("dotenv").config({
+  path: '.env.local'
+});
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require('bcryptjs');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const { ADMIN1_EMAIL, ADMIN1_PASSWORD } = process.env;
+
+  const hashedPassword = await bcrypt.hash(ADMIN1_PASSWORD, 12);
+
+  await prisma.admin.upsert({
+    where: { email: ADMIN1_EMAIL },
+    update: {},
+    create: {
+      email: ADMIN1_EMAIL,
+      password: hashedPassword,
+    },
+  });
+
+  console.log("Admin korisnik je ubačen u bazu.");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
---
+++ src/components/utils/env.js
@@ -0,0 +1,2 @@
+export const ADMIN1_EMAIL = process.env.ADMIN1_EMAIL;
+export const ADMIN1_PASSWORD = process.env.ADMIN1_PASSWORD;
