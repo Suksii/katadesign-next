2c4f9d4eed63a13253266f1056690ca50590a14e
unknown <ramovic225@gmail.com>
2025-11-26 21:16:55 +0100

    footer bg removed

--- src/components/wrappers/HomePageWrapper.jsx
+++ src/components/wrappers/HomePageWrapper.jsx
@@ -32,25 +32,6 @@ const HomePageWrapper = ({ children }) => {
           className="w-full h-[80px] md:h-auto"
         />
       </TextMotion>
-
-      <TextMotion
-        animation={{
-          initial: { opacity: 0, x: 150 },
-          animate: { opacity: 1, x: 0 },
-          transition: { duration: 1.2, delay: 3.5 },
-        }}
-        className="absolute bottom-0 max-w-[769px] right-0 md:right-[15%] z-0"
-      >
-        <Image
-          src="/bg-footer.png"
-          alt="bg-footer"
-          width={769}
-          priority
-          height={300}
-          className="w-full h-auto"
-        />
-      </TextMotion>
-
       <div className="relative">{children}</div>
     </div>
   );
