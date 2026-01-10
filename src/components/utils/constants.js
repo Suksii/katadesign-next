c8434cd8c3f42c43f7c0fa7143c75e84f07f0fb4
unknown <ramovic225@gmail.com>
2026-01-10 18:52:16 +0100

    news layout changed

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -54,7 +54,12 @@ export const newsData = [
     subtitleKey: "brskut_teaser",
     main_paragraphsKey: "brskut_desc_1",
     paragraphsKey: "brskut_desc_2",
-    banner_image: { src: "/brskut/Brskut0.jpg", alt: "1", width: 400, height: 800 },
+    banner_image: {
+      src: "/brskut/Brskut0.jpg",
+      alt: "1",
+      width: 400,
+      height: 800,
+    },
     main_images: [
       { src: "/1.jpg", alt: "1", width: 400, height: 800 },
       { src: "/2.jpg", alt: "2", width: 400, height: 800 },
@@ -69,6 +74,22 @@ export const newsData = [
     ],
     date: "28.08.2025",
     slug: "brskut",
+  },
+  {
+    id: "histora",
+    titleKey: "histora_naslov",
+    subtitleKey: "histora_teaser",
+    main_paragraphsKey: "histora_desc_1",
+    paragraphsKey: "histora_desc_2",
+    banner_image: {
+      src: "/histora/histora0.jpg",
+      alt: "1",
+      width: 400,
+      height: 800,
+    },
+    main_images: [{ src: "/1.jpg", alt: "1", width: 400, height: 800 }],
+    date: "28.08.2025",
+    slug: "histora",
   },
 ];
 
--- src/components/webpages/SingleBlog.jsx
+++ src/components/webpages/SingleBlog.jsx
@@ -10,35 +10,67 @@ const SingleBlog = ({ params }) => {
     {
       title: t("brskut_naslov"),
       subtitle: t("brskut_teaser"),
-      main_paragraphs: t.raw("brskut_desc_1"),
-      paragraphs: t.raw("brskut_desc_2"),
       banner_image: {
         src: "/brskut/Brskut0.jpg",
         alt: "1",
         width: 400,
         height: 800,
       },
-      main_images: [
-        { src: "/brskut/Brskut1.jpg", alt: "1", width: 400, height: 800 },
-        { src: "/brskut/Brskut2.jpg", alt: "2", width: 400, height: 800 },
+      date: "28.08.2025",
+      slug: "brskut",
+      sections: [
+        {
+          paragraphs: t.raw("brskut_desc_1"),
+          images: [
+            { src: "/brskut/Brskut1.jpg", alt: "1", width: 400, height: 800 },
+            { src: "/brskut/Brskut2.jpg", alt: "2", width: 400, height: 800 },
+          ],
+        },
+        {
+          paragraphs: t.raw("brskut_desc_2"),
+          images: [
+            { src: "/brskut/Brskut3.jpg", alt: "3", width: 400, height: 800 },
+            { src: "/brskut/Brskut4.jpg", alt: "4", width: 400, height: 800 },
+          ],
+          slider_images: [
+            { src: "/brskut/Brskut5.jpg", alt: "1", width: 400, height: 800 },
+            { src: "/brskut/Brskut6.jpg", alt: "2", width: 400, height: 800 },
+            { src: "/brskut/Brskut7.jpg", alt: "3", width: 400, height: 800 },
+          ],
+          secondaryImages: [
+            {
+              src: "/brskut/Brskut8.jpg",
+              alt: "1",
+              width: 400,
+              height: 800,
+            },
+          ],
+        },
       ],
-      blog_images: [
-        { src: "/brskut/Brskut3.jpg", alt: "3", width: 400, height: 800 },
-        { src: "/brskut/Brskut4.jpg", alt: "4", width: 400, height: 800 },
-      ],
-      slider_images: [
-        { src: "/brskut/Brskut5.jpg", alt: "1", width: 400, height: 800 },
-        { src: "/brskut/Brskut6.jpg", alt: "2", width: 400, height: 800 },
-        { src: "/brskut/Brskut7.jpg", alt: "3", width: 400, height: 800 },
-      ],
-      singleImage: {
-        src: "/brskut/Brskut8.jpg",
+    },
+    {
+      title: t("histora_naslov"),
+      subtitle: t("histora_teaser"),
+      banner_image: {
+        src: "/histora/histora0.jpg",
         alt: "1",
         width: 400,
         height: 800,
       },
       date: "28.08.2025",
-      slug: "brskut",
+      slug: "histora",
+      sections: [
+        {
+          title: t("histora_naslov_1"),
+          main_description: t.raw("histora_main_desc_1"),
+          paragraphs: t.raw("histora_desc_2"),
+          images: [{ src: "/histora/histora0.jpg", alt: "Histora" }],
+        },
+        {
+          title: t("histora_naslov_2"),
+          paragraphs: t.raw("histora_desc_2"),
+        },
+      ],
     },
   ];
 
@@ -47,11 +79,12 @@ const SingleBlog = ({ params }) => {
 
   return (
     <div className="w-full h-full">
-      <div className="relative w-full aspect-video">
+      <div className="w-full mt-2">
         <Image
           src={blog.banner_image}
-          alt="Placeholder"
-          fill
+          alt={blog.title}
+          width={1920}
+          height={855}
           className="object-cover"
         />
       </div>
@@ -60,51 +93,72 @@ const SingleBlog = ({ params }) => {
         <h3 className="w-full md:w-2/3 pb-24 text-center text-3xl text-gray-500 italic">
           {blog.subtitle}
         </h3>
-        <div className="flex flex-col gap-8 w-full md:w-1/2 mx-auto">
-          {blog.main_paragraphs.map((p, index) => (
-            <p key={index} className="text-xl">
-              {p}
-            </p>
-          ))}
-          <div className="flex flex-col xl:flex-row gap-4 mt-12">
-            {blog.main_images.map((image) => (
-              <div key={image.alt} className="relative w-full h-[500px]">
-                <Image
-                  src={image.src}
-                  alt={image.alt}
-                  fill
-                  className="object-cover"
-                />
+        <div className="flex flex-col">
+          {blog.sections.map((section, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col gap-8 w-full md:w-1/2 mx-auto"
+              >
+                {section.main_description && (
+                  <div>
+                    <h3 className="text-xl">{section.main_description}</h3>
+                  </div>
+                )}
+                {section.title && (
+                  <h3 className="text-xl font-semibold italic">
+                    {section.title}
+                  </h3>
+                )}
+                <div className="flex flex-col gap-8">
+                  {section.paragraphs.map((p, index) => (
+                    <p key={index} className="text-lg">
+                      {p}
+                    </p>
+                  ))}
+                </div>
+                {section.images && (
+                  <div className="flex flex-col xl:flex-row gap-4">
+                    {section.images.map((image) => (
+                      <div
+                        key={image.alt}
+                        className="relative w-full h-[500px]"
+                      >
+                        <Image
+                          src={image.src}
+                          alt={image.alt}
+                          fill
+                          className="object-cover"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
+                <div>
+                  {section.slider_images && (
+                    <Slider images={section.slider_images} slidesToShow={2} />
+                  )}
+                </div>
+                {section.secondaryImages && (
+                  <div className="flex flex-col xl:flex-row gap-4">
+                    {section.secondaryImages.map((image) => (
+                      <div
+                        key={image.alt}
+                        className="relative w-full h-[500px]"
+                      >
+                        <Image
+                          src={image.src}
+                          alt={image.alt}
+                          fill
+                          className="object-cover"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
-            ))}
-          </div>
-          {blog.paragraphs.map((p, index) => (
-            <p key={index} className="text-xl">
-              {p}
-            </p>
-          ))}
-          <div className="flex flex-col xl:flex-row gap-4 mt-12">
-            {blog.blog_images.map((image) => (
-              <div key={image.alt} className="relative w-full h-[500px]">
-                <Image
-                  src={image.src}
-                  alt={image.alt}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            ))}
-          </div>
-          <Slider images={blog.slider_images} slidesToShow={2} />
-
-          <div className="relative w-full aspect-video">
-            <Image
-              src={blog.singleImage}
-              alt="Placeholder"
-              fill
-              className="object-cover"
-            />
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
